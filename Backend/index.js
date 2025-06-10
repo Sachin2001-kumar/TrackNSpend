108785723d995f771b650fbc9982849f377f64d7
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-06-10 14:08:46 +0530

    add prisma

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,19 @@
-const express=require("express");
-const mongoose = require('mongoose'); 
-const cors=require("cors");
-const { db } = require("./db/db");
-const {readdirSync} = require('fs')
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { readdirSync } = require("fs");
+require("dotenv").config();
 
-
-
-const app=express();
+const app = express();
 app.use(cors());
 app.use(express.json());
-const Port=process.env.PORT || 5000;
+const Port = process.env.PORT || 5000;
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+readdirSync("./routes").map((route) =>
+  app.use("/api/v1", require("./routes/" + route))
+);
 
-
-app.listen(Port,(req,res)=>{
-    db()
-    console.log(`Server Listen at Port: ${Port}`);
-})
+app.listen(Port, (req, res) => {
+  console.log(`Server Listen at Port: ${Port}`);
+});
